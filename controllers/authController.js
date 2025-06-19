6b91b145a8d1989f8530871f01d65a91b35e1223
Suman Das <sum4ndas@gmail.com>
2025-06-20 05:20:58 +0530

    Add login controller to authenticate user with passport and generate JWT token on successful login.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const prisma = require("../config/prismaClient");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
+const passport = require("../config/passport");
+const jwt = require("jsonwebtoken");
 
 const { body, validationResult } = require("express-validator");
 
@@ -42,4 +44,29 @@ exports.signup = [
 
     res.status(201).json({ message: "User created.", user: user });
   }),
+];
+
+exports.login = [
+  validateUser,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array(), input: req.body });
+    }
+
+    passport.authenticate("local", { session: false }, (err, user, info) => {
+      if (err || !user) {
+        return res
+          .status(400)
+          .json({ message: "Invalid credentials", user: null });
+      }
+
+      const payload = { id: user.id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1d",
+      });
+
+      return res.json({ message: "Login successful", token });
+    })(req, res, next);
+  },
 ];
