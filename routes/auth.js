49d7fdb170b62ee067170de7474539b5e2c9819c
Suman Das <sum4ndas@gmail.com>
2025-06-20 05:21:35 +0530

    Add login route

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = Router();
 const authController = require("../controllers/authController");
 
 router.post("/auth/signup", authController.signup);
+router.post("/auth/login", authController.login);
 
 module.exports = router;
