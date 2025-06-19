5e22ca72cc4fbf1c7cfdec142bbe364b4c502853
Suman Das <sum4ndas@gmail.com>
2025-06-19 07:24:33 +0530

    Create app.js and add start and dev scripts

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Handle route not found
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error("Error stack", err.stack);
+
+  res.status(err.status || 500).json({
+    error: err.message || "Internal server error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("app is running on port: ", PORT));
