bbb3680cc6591f71e87068083237e4f454838c84
Suman Das <sum4ndas@gmail.com>
2025-06-21 04:21:27 +0530

    Add getPostById controller and add '/posts/postId' GET route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -10,3 +10,26 @@ exports.getAllPosts = asyncHandler(async (req, res) => {
 
   res.status(200).json({ posts });
 });
+
+exports.getPostById = asyncHandler(async (req, res) => {
+  const postId = Number(req.params.postId);
+  if (isNaN(postId)) {
+    return res.status(400).json({ error: "Invalid post ID" });
+  }
+
+  const post = await prisma.post.findUnique({
+    where: { id: postId },
+    include: {
+      author: {
+        select: { id: true, name: true, email: true },
+      },
+      comments: true,
+    },
+  });
+
+  if (!post) {
+    return res.status(404).json({ error: `Post with ID ${postId} not found` });
+  }
+
+  res.status(200).json({ post });
+});
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = Router();
 const postsController = require("../controllers/postsController");
 
 router.get("/posts", postsController.getAllPosts);
+router.get("/posts/:postId", postsController.getPostById);
 
 module.exports = router;
