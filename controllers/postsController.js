18395e0c78a30d9c38f724dcbb1a1456adfd32a6
Suman Das <sum4ndas@gmail.com>
2025-06-21 08:36:23 +0530

    Add deletePostById controller and add '/posts/:postID' DELETE route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -97,3 +97,23 @@ exports.updatePostById = [
     }
   }),
 ];
+
+exports.deletePostById = asyncHandler(async (req, res) => {
+  const postId = Number(req.params.postId);
+  if (isNaN(postId)) {
+    return res.status(400).json({ error: "Invalid post ID" });
+  }
+
+  try {
+    await prisma.post.delete({
+      where: { id: postId, authorId: req.user.id },
+    });
+
+    res.status(200).json({ message: `Post with ID: ${postId} deleted` });
+  } catch (err) {
+    if (err.code === "P2025") {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    throw err;
+  }
+});
--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,6 @@ router.get("/posts", postsController.getAllPosts);
 router.post("/posts", postsController.createPost);
 router.get("/posts/:postId", postsController.getPostById);
 router.put("/posts/:postId", postsController.updatePostById);
+router.delete("/posts/:postId", postsController.deletePostById);
 
 module.exports = router;
