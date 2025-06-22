09592b47d001ee9f0c555ff9be04896bbc8eb785
Suman Das <sum4ndas@gmail.com>
2025-06-22 12:06:59 +0530

    Create createComment controller with data validation

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,6 +1,15 @@
 const asyncHandler = require("express-async-handler");
 const prisma = require("../config/prismaClient");
 
+const { body, validationResult } = require("express-validator");
+
+const validateComment = [
+  body("content")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("Comment must be more than 4 characters"),
+];
+
 exports.getAllCommentsByPostId = asyncHandler(async (req, res) => {
   const postId = Number(req.params.postId);
   if (isNaN(postId)) {
@@ -37,3 +46,44 @@ exports.getCommentById = asyncHandler(async (req, res) => {
 
   res.status(200).json({ comment });
 });
+
+exports.createComment = [
+  validateComment,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array(),
+        input: req.body,
+      });
+    }
+
+    const postId = Number(req.params.postId);
+    if (isNaN(postId)) {
+      return res.status(400).json({ error: "Invalid postId" });
+    }
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ error: `Post not found with ID: ${postId}` });
+    }
+
+    const comment = await prisma.comment.create({
+      data: {
+        content: req.body.content,
+        postId: postId,
+        authorId: req.user.id,
+      },
+    });
+
+    res.status(201).json({
+      message: "Comment created successfully",
+      comment,
+    });
+  }),
+];
