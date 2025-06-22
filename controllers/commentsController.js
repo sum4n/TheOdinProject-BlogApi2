4c27cdac37145a485668064fd13f767a8a979fdf
Suman Das <sum4ndas@gmail.com>
2025-06-22 13:17:40 +0530

    Create updateCommentById controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -87,3 +87,44 @@ exports.createComment = [
     });
   }),
 ];
+
+exports.updateCommentById = [
+  validateComment,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array(), input: req.body });
+    }
+
+    const postId = Number(req.params.postId);
+    const commentId = Number(req.params.commentId);
+
+    if (isNaN(postId)) {
+      return res.status(400).json({ error: "Invalid post ID" });
+    }
+    if (isNaN(commentId)) {
+      return res.status(400).json({ error: "Invalid comment ID" });
+    }
+
+    const comment = await prisma.comment.findFirst({
+      where: { id: commentId, postId },
+    });
+
+    if (!comment) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+    if (comment.authorId !== req.user.id) {
+      return res.status(403).json({ error: "Not Authorized" });
+    }
+
+    const updatedComment = await prisma.comment.update({
+      where: { id: commentId },
+      data: { content: req.body.content },
+    });
+
+    res.status(200).json({
+      message: `Comment with ID: ${commentId} updated`,
+      comment: updatedComment,
+    });
+  }),
+];
